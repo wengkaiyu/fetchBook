c5b8aa187cb0e534d30da4c58364095f284128e2
wengkaiyu <wengkaiyu@gmail.com>
2025-03-15 18:20:32 +0800

    debug

--- main.py
+++ main.py
@@ -218,6 +218,7 @@ def generate_toc(chapters):
             </nav>
         </body>
     </html>'''
+    print(toc_content)
     with open("toc.xhtml", 'w', encoding='utf-8') as f:
         f.write(toc_content)
 
